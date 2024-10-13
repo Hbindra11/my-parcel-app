02c5ab1fe446c35fcf00ffe300c8d95ad4aa7718
Hema Bindra <hbindra11@yahoo.com>
2024-10-13 14:14:31 +0200

    copied js from app file on github

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-import * as Leaflet from 'leaflet';
- 
-console.log(Leaflet);
+
 import Leaflet from 'leaflet'; // import everything from leaflet
 import 'leaflet/dist/leaflet.css'; // import leaflet css
- 
+
 const WBS = [52.457131, 13.54007]; // WBS coordinates
 const map = Leaflet.map('map').setView(WBS, 13); // create a map object with a center and zoom level
 const markerIcon = Leaflet.icon({
@@ -11,12 +9,12 @@ const markerIcon = Leaflet.icon({
   iconAnchor: [10, 20]
 }); // There was an issue with the default marker icon, so we create a new one
 Leaflet.marker(WBS, { icon: markerIcon }).addTo(map); // add a marker to the map at the WBS coordinates
- 
+
 Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map); // add a tile layer to the map, the tiles are those images that make up the map
-
+// My locations
 const myLocations = [
   {
     name: 'WBS CODING SCHOOL',
@@ -45,6 +43,7 @@ myLocations.forEach(location => {
     .bindPopup(location.description)
     .addTo(map);
 });
+
 // Set the view to the bounds of all markers
 const bounds = Leaflet.latLngBounds(myLocations.map(location => location.location));
 map.fitBounds(bounds);
